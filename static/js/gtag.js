bd295c7b38986c8cfa7bdd19098c703fa3e653fb
codebeaker <sonalimander@gmail.com>
2025-01-31 09:50:19 -0800

    initialize adding google analytics code

---
+++ static/js/gtag.js
@@ -0,0 +1,12 @@
+$(() => {
+
+  /* Default installation */
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    dataLayer.push(arguments);
+  };
+  gtag('js', new Date());
+  gtag('config', 'G-09L5P1T9G3');
+
+});
